8ab347916ba39d163b69c0e67644648fd8b46f95
Amin <amin.esmkhani75@gmail.com>
2021-07-12 11:27:20 +0430

    Add Recaptcha In FrontEndgit add .

--- config/services.php
+++ config/services.php
@@ -30,4 +30,8 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
+    'recaptcha' => [
+        'site_key' => env('RECAPTCHA_SITE_KEY'),
+        'secret_key' => env('RECAPTCHA_SECRET_KEY')
+    ]
 ];
